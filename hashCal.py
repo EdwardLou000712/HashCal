813848b4089c950ca278a4d9497b5bae2d461bc6
EdwardLou000712 <ed0712lou@gmail.com>
2026-02-24 17:55:08 +0900

    create hash calculator script

---
+++ hashCal.py
@@ -0,0 +1,17 @@
+import hashlib
+
+def calculate_file_hash(filepath):
+    sha256 = hashlib.sha256()
+    
+    with open(filepath, "rb") as f:
+        while chunk := f.read(8192):
+            sha256.update(chunk)
+    
+    return sha256.hexdigest()
+
+
+if __name__ == "__main__":
+    file_path = "/Users/shutonglou/Desktop/Protools session file test/gt t_副本2.ptx"
+    file_hash = calculate_file_hash(file_path)
+    print("SHA256:", file_hash)
+
