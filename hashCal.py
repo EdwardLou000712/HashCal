cea6597a22dcc1165e24b7bf5c45a8607d3298d6
EdwardLou000712 <ed0712lou@gmail.com>
2026-02-24 17:56:52 +0900

    Update hashCal.py

--- hashCal.py
+++ hashCal.py
@@ -11,7 +11,7 @@ def calculate_file_hash(filepath):
 
 
 if __name__ == "__main__":
-    file_path = "/Users/shutonglou/Desktop/Protools session file test/gt t_副本2.ptx"
+    file_path = "your path file"
     file_hash = calculate_file_hash(file_path)
     print("SHA256:", file_hash)
 
